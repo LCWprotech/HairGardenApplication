0ab76c7adb38c04b2e2a315900caa7268a7da956
CHAI WEI NING <weining2001@gmail.com>
2021-10-26 22:11:25 +0800

    WN 26/10/2021 10.11 Customer Appointment

--- app/src/main/java/com/LCWprotech/hairgardenapplication/Customer/Appointment.java
+++ app/src/main/java/com/LCWprotech/hairgardenapplication/Customer/Appointment.java
@@ -6,16 +6,28 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TimePicker;
 
 import com.LCWprotech.hairgardenapplication.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class Appointment extends AppCompatActivity {
     DrawerLayout drawerLayout;
     ImageView btMenu;
     RecyclerView recyclerView;
+    EditText date_in;
+    EditText time_in;
+    EditText date_time_in;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +36,13 @@ public class Appointment extends AppCompatActivity {
         drawerLayout = findViewById(R.id.drawer_layout);
         btMenu = findViewById(R.id.bt_menu);
         recyclerView = findViewById(R.id.recycler_view);
+        date_in=findViewById(R.id.date_input);
+        time_in=findViewById(R.id.time_input);
+        date_time_in=findViewById(R.id.date_time_input);
+
+        date_time_in.setInputType(InputType.TYPE_NULL);
+        date_in.setInputType(InputType.TYPE_NULL);
+        time_in.setInputType(InputType.TYPE_NULL);
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
@@ -35,9 +54,34 @@ public class Appointment extends AppCompatActivity {
                 drawerLayout.openDrawer(GravityCompat.START);
             }
         });
+
+        date_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDateDialog(date_in);
+            }
+        });
+
     }
     protected void onPause(){
         super.onPause();
         Customer.closeDrawer(drawerLayout);
+    }
+
+    private void showDateDialog(final EditText date_in) {
+        final Calendar calendar=Calendar.getInstance();
+        DatePickerDialog.OnDateSetListener dateSetListener=new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                calendar.set(Calendar.YEAR,year);
+                calendar.set(Calendar.MONTH,month);
+                calendar.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+                SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yy-MM-dd");
+                date_in.setText(simpleDateFormat.format(calendar.getTime()));
+
+            }
+        };
+
+        new DatePickerDialog(Appointment.this,dateSetListener,calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH)).show();
     }
 }
