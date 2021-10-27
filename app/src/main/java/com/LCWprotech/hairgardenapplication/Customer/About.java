ba7734bee3a49c5dee202459dd5b0d85279a2bc8
Wong Lik <81523243+wonglik@users.noreply.github.com>
2021-10-27 09:51:12 +0800

    Wong Lik 21/10/27 09:50 Privacy Policy

--- app/src/main/java/com/LCWprotech/hairgardenapplication/Customer/About.java
+++ app/src/main/java/com/LCWprotech/hairgardenapplication/Customer/About.java
@@ -6,16 +6,26 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.LCWprotech.hairgardenapplication.ForgotPassword;
+import com.LCWprotech.hairgardenapplication.Login;
+import com.LCWprotech.hairgardenapplication.MainMenu;
 import com.LCWprotech.hairgardenapplication.R;
+import com.LCWprotech.hairgardenapplication.Registration;
 
 public class About extends AppCompatActivity {
+    TextView txtPrivacy;
     DrawerLayout drawerLayout;
     ImageView btMenu;
     RecyclerView recyclerView;
+    Activity activity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +34,7 @@ public class About extends AppCompatActivity {
         drawerLayout = findViewById(R.id.drawer_layout);
         btMenu = findViewById(R.id.bt_menu);
         recyclerView = findViewById(R.id.recycler_view);
+        txtPrivacy = (TextView) findViewById(R.id.text2);
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
@@ -35,9 +46,24 @@ public class About extends AppCompatActivity {
                 drawerLayout.openDrawer(GravityCompat.START);
             }
         });
+/*
+        txtPrivacy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                activity.startActivity(new Intent(activity, PrivacyPolicy.class));
+                finish();
+            }
+        });*/
+
     }
     protected void onPause(){
         super.onPause();
         Customer.closeDrawer(drawerLayout);
     }
+/*
+    public void Privacy(View v){
+        Intent z = new Intent(activity, Appointment.class);
+        activity.startActivity(z);
+        finish();
+    }*/
 }
--- app/src/main/java/com/LCWprotech/hairgardenapplication/Customer/Appointment.java
+++ app/src/main/java/com/LCWprotech/hairgardenapplication/Customer/Appointment.java
@@ -6,29 +6,18 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.app.DatePickerDialog;
-import android.app.TimePickerDialog;
 import android.os.Bundle;
-import android.text.InputType;
 import android.view.View;
-import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TimePicker;
 
 import com.LCWprotech.hairgardenapplication.R;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 
 public class Appointment extends AppCompatActivity {
     DrawerLayout drawerLayout;
     ImageView btMenu;
     RecyclerView recyclerView;
-    EditText date_in;
-    EditText time_in;
-    EditText date_time_in;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,13 +25,6 @@ public class Appointment extends AppCompatActivity {
         drawerLayout = findViewById(R.id.drawer_layout);
         btMenu = findViewById(R.id.bt_menu);
         recyclerView = findViewById(R.id.recycler_view);
-        date_in=findViewById(R.id.date_input);
-        time_in=findViewById(R.id.time_input);
-        date_time_in=findViewById(R.id.date_time_input);
-
-        date_time_in.setInputType(InputType.TYPE_NULL);
-        date_in.setInputType(InputType.TYPE_NULL);
-        time_in.setInputType(InputType.TYPE_NULL);
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
@@ -55,12 +37,6 @@ public class Appointment extends AppCompatActivity {
             }
         });
 
-        date_in.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showDateDialog(date_in);
-            }
-        });
 
     }
     protected void onPause(){
@@ -68,20 +44,4 @@ public class Appointment extends AppCompatActivity {
         Customer.closeDrawer(drawerLayout);
     }
 
-    private void showDateDialog(final EditText date_in) {
-        final Calendar calendar=Calendar.getInstance();
-        DatePickerDialog.OnDateSetListener dateSetListener=new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                calendar.set(Calendar.YEAR,year);
-                calendar.set(Calendar.MONTH,month);
-                calendar.set(Calendar.DAY_OF_MONTH,dayOfMonth);
-                SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yy-MM-dd");
-                date_in.setText(simpleDateFormat.format(calendar.getTime()));
-
-            }
-        };
-
-        new DatePickerDialog(Appointment.this,dateSetListener,calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH)).show();
-    }
 }
